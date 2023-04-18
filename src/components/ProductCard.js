b482f515e662af158168f3a439b3a39118ff9595
Sin Sreyphea <sreypheasin@gmail.com>
2023-04-18 22:20:03 +0700

    edit button

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,12 +6,12 @@ export default function ProductCard({product}) {
     <div className='bg-white shadow-lg mx-auto w-64 rounded-lg p-3 mt-10 '>
         <div  >
             <img src={product.images} className='object-cover w-full rounded-lg'></img>
-            <h2 className='mb-2 line-clamp-1 mt-2 text-2xl opacity-75 text-black line-clamp-2 '>{product.title}</h2>
-            <p className='mb-5 text-base line-clamp-2 opacity-50 overflow-hidden'>{product.description}</p>
+            <h2 className='mb-2 line-clamp-1 mt-2 text-2xl opacity-75 text-black line-clamp-1'>{product.title}</h2>
+            <p className='mb-5 text-base line-clamp-3 opacity-50 overflow-hidden'>{product.description}</p>
 
         </div>
         <div className='flex justify-between '>
-            <Button title='Buy'/>
+            <Button color={"#1e40af"} title='Buy'/>
             <Button title='Add to cart'/>
         </div>
     </div>
--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Button({title}) {
+export default function Button({title,color}) {
   return (
-    <button className='text-white bg-blue-950 p-1 pl-4 pr-4 rounded-lg hover:bg-blue-900' >{title}</button>
+    <button style={{backgroundColor: color}} className='text-white bg-blue-950 p-1 pl-4 pr-4 rounded-lg hover:bg-blue-900' >{title}</button>
   )
 }
